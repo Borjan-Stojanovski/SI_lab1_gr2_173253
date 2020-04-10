dbeb8f6fd8a1f3d0688caf3568826c9091907783
Stojanovski <brainstererrors@gmail.com>
2020-04-10 22:18:16 +0200

    added Faculty.java and changed students.csv

---
+++ Faculty.java
@@ -0,0 +1,20 @@
+class Faculty {
+	String index;
+	String firstName;
+	String lastName;
+	List<a> grades;
+	grades = [5, 5, 5, 6, 6, 10];
+	//TODO constructor
+
+	//TODO seters & getters
+
+	public double getAverage() {
+		//TODO
+		get_average;
+	}
+
+	public int ECTSCredits() {
+		//TODO
+		ectscredits;
+	}
+}
